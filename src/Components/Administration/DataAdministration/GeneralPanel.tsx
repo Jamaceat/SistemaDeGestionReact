4b808f18cb2093ca461caf84bfc71c42d96f37b2
Johan <johanoriginalplus@gmail.com>
2023-01-20 01:00:47 -0500

    Pagination and Search finished

--- src/Components/Administration/DataAdministration/GeneralPanel.tsx
+++ src/Components/Administration/DataAdministration/GeneralPanel.tsx
@@ -1,21 +1,27 @@
 import {
 	Button,
+	CircularProgress,
 	FormControl,
 	Grid,
+	Pagination,
+	Paper,
 	SxProps,
 	TextField,
 	Typography,
 } from "@mui/material"
 import {Box} from "@mui/system"
-import React, {useEffect, useState} from "react"
+import React, {useEffect, useRef, useState} from "react"
 import axios from "axios"
 import PaginationI from "../../../../Interfaces/Pagination"
+import ItemInventario from "./Item"
 
 interface panelStyles {
 	container: SxProps
 	subContainer: SxProps
 	AddContainer: SxProps
 	searchContainer: SxProps
+	itemPanel: SxProps
+	progressContainer: SxProps
 }
 
 const style: panelStyles = {
@@ -32,32 +38,78 @@ const style: panelStyles = {
 			height: "5rem",
 			padding: "3rem",
 		},
-		"@media (min-width:0px)": {width: "100%"},
+		"@media (min-width:0px)": {width: "100%", paddingY: "3rem"},
 		marginBottom: "1rem",
 	},
-	searchContainer: {width: "100%"},
+	searchContainer: {width: "100%", marginBottom: "1rem"},
+	itemPanel: {
+		width: "100%",
+		backgroundColor: "#3496F7",
+		boxSizing: "border-box",
+		padding: "1rem",
+	},
+	progressContainer: {
+		display: "flex",
+		height: "50vh",
+		width: "100%",
+		justifyContent: "center",
+		alignItems: "center",
+	},
 }
 
 function Panel() {
+	const inputRef = useRef() as React.MutableRefObject<HTMLDivElement>
+
 	// const initialState: PaginationI = {items:[]}
 	const [pages, setPagination] = useState<PaginationI | null>(null)
+	const [page, setPage] = useState<number>(1)
+	const [count, setCount] = useState<number>(1)
+	const [lookUp, setlookUp] = useState<string>("")
+	const [noInformation, setNoInformation] = useState<boolean>(false)
 
 	const fetchingData = async () => {
 		try {
+			if (lookUp !== "") {
+				setPage((prev) => 1)
+			}
 			let data = await axios.get(
-				"https://ops.enerbit.dev/learning/api/v1/meters?page=0&size=10"
+				`https://ops.enerbit.dev/learning/api/v1/meters?page=${
+					page - 1
+				}&size=10${lookUp !== "" ? `&serial=${lookUp}` : ""}`
 			)
+			let info: PaginationI = data.data
 
-			setPagination((prev) => data.data)
-			console.log(data, "yyy", pages)
+			setPagination((prev) => info)
+			setCount((prev) => info.pages)
 		} catch (e) {
-			console.log(e)
+			setPagination((prev) => null)
+			setCount((prev) => 0)
+			setTimeout(() => {
+				setNoInformation((prev) => true)
+			}, 1000)
 		}
 	}
 
+	// Recuperacion de datos en la base de datos
 	useEffect(() => {
+		console.log("looking up", lookUp)
 		fetchingData()
-	}, [])
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [page, lookUp])
+
+	// state para la paginación
+	const handleChange = (evt: React.ChangeEvent<unknown>, value: number) => {
+		setPage((prev) => value)
+	}
+
+	const handleInputChange = () => {
+		const value = extractInputValue()
+		setlookUp((prev: string) => value)
+	}
+
+	const extractInputValue = (): any => {
+		return inputRef?.current.querySelector("input")?.value
+	}
 
 	return (
 		<Grid container sx={{...style.container}}>
@@ -68,20 +120,50 @@ function Panel() {
 					</Button>
 				</Box>
 				<FormControl sx={{...style.searchContainer}}>
-					<TextField helperText={"Buscar por Serial"} />
+					<Typography
+						fontFamily={"Tauri, sans-serif"}
+						fontSize={"3vmin"}
+						variant="h5"
+						marginBottom={".5rem"}
+					>
+						Busqueda por serial
+					</Typography>
+					<TextField ref={inputRef} onChange={handleInputChange} />
 				</FormControl>
+				<Paper component={Box} sx={{...style.itemPanel}}>
+					<Pagination
+						color="primary"
+						page={page}
+						count={count}
+						onChange={handleChange}
+						boundaryCount={2}
+					/>
+					{pages === null ? (
+						<Box sx={{...style.progressContainer}}>
+							{!noInformation ? (
+								<CircularProgress color="secondary" />
+							) : (
+								<Typography variant="h3" fontSize={"5vmin"}>
+									No information
+								</Typography>
+							)}
+						</Box>
+					) : (
+						<>
+							{pages.items.map((x) => (
+								<ItemInventario key={x.id} {...x} />
+							))}
+						</>
+					)}
+					<Pagination
+						color="primary"
+						page={page}
+						count={count}
+						onChange={handleChange}
+						boundaryCount={2}
+					/>
+				</Paper>
 			</Grid>
-			{pages === null ? (
-				<Typography>Esta null</Typography>
-			) : (
-				<>
-					{" "}
-					{console.log(pages)}
-					{pages.items.map((x) => (
-						<Typography key={x.id}>{x.condition} ' </Typography>
-					))}
-				</>
-			)}
 		</Grid>
 	)
 }
--- src/Components/Administration/DataAdministration/Item.tsx
+++ src/Components/Administration/DataAdministration/Item.tsx
@@ -1,8 +1,70 @@
+import {Button, Grid, Paper, SxProps, Typography} from "@mui/material"
 import React from "react"
 import singleItem from "../../../../Interfaces/SingleItem"
 
+interface itemStyle {
+	container: SxProps
+	buttonsContainer: SxProps
+	textContainer: SxProps
+}
+
+const style: itemStyle = {
+	container: {
+		width: "100%",
+		marginY: ".5rem",
+		padding: ".5rem",
+		display: "flex",
+		borderRadius: "1rem",
+
+		flexWrap: "wrap",
+		height: "auto",
+	},
+	buttonsContainer: {display: "flex", alignItems: "stretch"},
+	textContainer: {
+		display: "flex",
+		alignItems: "center",
+		fontSize: "1rem",
+		flexGrow: "1",
+	},
+}
+
 function ItemInventario(props: singleItem) {
-	return <></>
+	const {
+		id,
+		// condition,
+		// connection_type,
+		// created_at,
+		// i_b,
+		// i_max,
+		// i_n,
+		// location,
+		// manufacturer,
+		// owner,
+		// purchase,
+		// seals,
+		// serial,
+		// storage_system,
+		// updated_at,
+	} = props
+
+	return (
+		<Grid container sx={{...style.container}} component={Paper} elevation={4}>
+			<Grid
+				item
+				component={Typography}
+				variant="h6"
+				fontFamily={"Tauri, sans-serif"}
+				height={"auto"}
+				sx={{...style.textContainer}}
+			>
+				Item ID {id}
+			</Grid>
+			<Grid item sx={{...style.buttonsContainer}}>
+				<Button>Edit</Button>
+				<Button color="error">Delete</Button>
+			</Grid>
+		</Grid>
+	)
 }
 
 export default ItemInventario
